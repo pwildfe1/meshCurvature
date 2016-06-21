5a11b6a74b21286885f4fb8695159931a23c008e
Peter Wildfeuer <wildfeuer.peter@gmail.com>
2016-06-21 10:28:57 -0400

    just added comments to the original script for easy following

--- copyVertexColorsOver.py
+++ copyVertexColorsOver.py
@@ -1,7 +1,14 @@
 import rhinoscriptsyntax as rs
 import math as m
 
+######## THIS SCRIPT PULLS COLORS FROM ONE MESH TO ANOTHER ##########
+# This helps if you modify a mesh through splitting and color is lost
+# you can overalay that modified mesh on its original and the colors
+# will transfer over.
+
 def Main():
+    #sourceMesh is the original mesh, newMesh is the mesh we want to copy
+    #the colors to.
     sourceMesh = rs.GetObject("select source mesh for colors",rs.filter.mesh)
     newMesh = rs.GetObject("select mesh to change",rs.filter.mesh)
     srcVertices = rs.MeshVertices(sourceMesh)
@@ -11,10 +18,12 @@ def Main():
     for i in range(len(vertices)):
         filtered = []
         indexes = []
+        # This section filters out far vertices to save computing time
         for j in range(len(srcVertices)):
             if rs.Distance(srcVertices[j],vertices[i])<1:
                 filtered.append(srcVertices[j])
                 indexes.append(j)
+        ##############################################################
         filteredIndex = rs.PointArrayClosestPoint(filtered,vertices[i])
         index = indexes[filteredIndex]
         newColors.append(srcColors[index])
--- meshCurvatureAnalysis.py
+++ meshCurvatureAnalysis.py
@@ -1,21 +1,34 @@
 import rhinoscriptsyntax as rs
 import math as m
 
+#### SCRIPT PURPOSE####
+# this script uses an overlayed grid on a mesh
+# to measure the curvature of each grid line at its
+# respective intersection points. Using those points
+# as reference, curvature values are assigned to the 
+# meshes vertices and turned into colors
+
 class contours:
-    def __init__(self,EVALPTS,BASECRV,VCRVS,RESO):
+    def __init__(self,EVALPTS,BASECRV,VCRVS):
+        #This class takes a single Ucrv all of the horizontal
+        #oriented Vcrvs the intersection points between them
         self.pts = EVALPTS
         self.crv = BASECRV
         self.vCrvs = VCRVS
-        self.reso = RESO
         self.uVals = []
         self.vVals = []
         self.colors = []
         self.position = rs.PointAdd([0,0,0],[0,0,0])
+        #This section finds the center position of the baseCrv#
         for i in range(len(self.pts)):
             self.position = rs.PointAdd(self.position,self.pts[i])
         self.position = self.position/len(self.pts)
+        #######################################################
     def locateVCrvs(self):
         sortedV = []
+        #Function generates a list of Vcrvs that match up to
+        #each intersection (grid point) on the Ucrv(baseCrv)
+        #being evaluated
         for i in range(len(self.pts)):
             minDist = 10000000
             for j in range(len(self.vCrvs)): 
@@ -26,15 +39,21 @@ class contours:
                     minDist = dist
                     closest = j
             sortedV.append(self.vCrvs[closest])
+        #sortedV is the list of Vcrvs that is closest to each evalPt
         return sortedV
     def uCurvature(self):
+        #This fuction retrieves the curvature value in the U direction
+        #for each point on the baseCrv (Ucrv)
         Curvatures = []
         for i in range(len(self.pts)):
             param = rs.CurveClosestPoint(self.crv,self.pts[i])
             Curvatures.append(rs.CurveCurvature(self.crv,param)[3])
         return Curvatures
     def vCurvature(self):
+        #This fuction retrieves the curvature value in the V direction
+        #for each point on the baseCrv(Ucrv)
         Curvatures = []
+        #Each point along the baseCrv is assigned a corresponding Vcrv
         sortedV = self.locateVCrvs()
         for i in range(len(self.pts)):
             param = rs.CurveClosestPoint(sortedV[i],self.pts[i])
@@ -50,10 +69,13 @@ def curvekey(curve):
     return point[2]
 
 def SortCurvesByZ(curves):
+    #Curves in the horizontal (V direction) are sorted by height
     sorted_curves = sorted(curves, key=curvekey)
     return sorted_curves
 
 def SortCurvesByPt(curves,pt):
+    #Curves in the vertical (U direction) are sorted from their distance
+    #from a reference point
     indexes = []
     distances = []
     sorted = []
@@ -74,10 +96,12 @@ def SortCurvesByPt(curves,pt):
     return sorted
 
 def Main():
+    #### A pre-existing mesh and projected gridlines are selected in rhino 
     mesh = rs.GetObject("please select mesh",rs.filter.mesh)
     uCrvs = rs.GetObjects("please select vertical contours",rs.filter.curve)
     vCrvs = rs.GetObjects("please select horizontal contours",rs.filter.curve)
     refPt = rs.GetObject("please enter reference point",rs.filter.point)
+    #### Ucrvs and Vcrvs are ordered by the ref point and height respectively
     uCrvs = SortCurvesByPt(uCrvs,refPt)
     vCrvs = SortCurvesByZ(vCrvs)
     myContours = []
@@ -87,10 +111,12 @@ def Main():
     red = []
     green = []
     colors = []
+    #Grid Curves in V are copied and rebuilt as degree 3 to measure curvature
     for i in range(len(vCrvs)):
         rebuild = rs.CopyObject(vCrvs[i])
         rs.RebuildCurve(rebuild,3,300)
         rebuiltV.append(rebuild)
+    #uCrvs intersect with vCrvs to gather up intersection points
     for i in range(len(uCrvs)):
         evalPts=[]
         for j in range(len(vCrvs)):
@@ -101,17 +127,28 @@ def Main():
                         evalPts.append(intersectionPts[i][1])
         rebuildU = rs.CopyObject(uCrvs[i])
         rs.RebuildCurve(rebuildU,3,300)
-        myContours.append(contours(evalPts,rebuildU,rebuiltV,.01))
+        #the Ucrv is rebuilt as degree 3 to measure curvature
+        #A new contour object is created with rebuilt Ucrv, rebuilt Vcrvs
+        #and intersection points
+        myContours.append(contours(evalPts,rebuildU,rebuiltV))
     maxRed = 0
     maxGreen = 0
     for i in range(len(myContours)):
         positions.append(myContours[i].position)
     for i in range(len(vertices)):
+        #Using the contour objects positions the verticies find their nearest
+        #contour object... and then find the nearest grid point to reduce
+        #computing time
         contourIndex = rs.PointArrayClosestPoint(positions,vertices[i])
         indexEvalPt = rs.PointArrayClosestPoint(myContours[contourIndex].pts,vertices[i])
+        #Curvatures in the U and V direction are asigned to each vertex
         myContours[contourIndex].collectUV()
+        #The U and V curvatures are turned into r and g values respectively
         red.append(myContours[contourIndex].uVals[indexEvalPt])
         green.append(myContours[contourIndex].vVals[indexEvalPt])
+    #After all the r and g values are found for the entire mesh
+    #The values are remapped to the 0-255 range and rgb values are stored
+    #in a list called colors
     for i in range(len(vertices)):
         if red[i]>maxRed:
             maxRed = red[i]
@@ -121,6 +158,7 @@ def Main():
         red[i] = red[i]/(maxRed+.001)*255
         green[i] = green[i]/(maxGreen+.001)*255
         colors.append([red[i],green[i],0])
+    #the list of rgb values creates the colors on the mesh at each vertex
     rs.MeshVertexColors(mesh,colors)
 
 Main()
